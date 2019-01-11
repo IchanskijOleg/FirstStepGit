0e4e7ec44c4a1a9b09bd258c6b9c926f2cad7885
Oleg <ichanskij@gmail.com>
2019-01-11 17:06:37 +0200

    First commit

---
+++ ConAppAreaFigures/ConAppAreaFigures/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAreaFigures
+{
+    class Triangle : Shape
+    {
+        private int a, h;
+        private double area;
+
+        public Triangle(int a, int h)
+        {
+            this.a = a;
+            this.h = h;
+        }
+        public override double GetArea()
+        {
+            area = 0.5 * a * h;
+            Console.WriteLine("Площа трикутника = " + area);
+            return area;
+        }
+    }
+}

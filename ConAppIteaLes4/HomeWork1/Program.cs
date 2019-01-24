ec678634dc8aeb79f4ea5268e783c51886febcb3
Ksenia <ichanskij@gmail.com>
2019-01-25 00:01:24 +0200

    add matrix

---
+++ ConAppIteaLes4/HomeWork1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        /*Задача №1
+            Выведите на экран строки вида:
+            *******
+            ****
+            *******
+            ****
+            *******
+            ****
+            (всего n строк, звездочек или 7, или 4 по очереди)
+        */
+        static void Main(string[] args)
+        {
+            const int asterisk7 = 7,
+                      asterisk4 = 4;
+            int n = 0;
+
+            Console.WriteLine("Уведіть кількість рядків");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(new string('*', asterisk4));
+                }
+                else
+                {
+                    Console.WriteLine(new string('*', asterisk7));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
